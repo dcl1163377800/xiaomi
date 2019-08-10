703fcd9c0bb1cc04b951fc70c3fb88df0b59b9a7
dingChengLin <1163377800@qq.com>
2019-08-10 16:25:18 +0800

    首页已经完成

---
+++ mi/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+      <Index></Index>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Index from "./view/HomePage/index.vue"
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Index
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ mi/src/view/HomePage/index.vue
@@ -0,0 +1,62 @@
+<template>
+    <div id="app">
+
+        <header>宇宙第一帅</header>
+        <main></main>
+        <footer></footer>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    html {
+    font-size: -webkit-calc(100vw/7.5);
+    font-size: calc(100vw / 7.5);
+    }
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    }
+    li {
+    list-style: none;
+    }
+    img {
+    vertical-align: top;
+    }
+    a {
+    text-decoration: none;
+    }
+    #app {
+    width: 100vw;
+    height: 100vh;
+    display: -webkit-flex;
+    display: flex;
+    flex-direction: column;
+         header{
+            height: 2.5rem;
+            background: red;
+            text-align: center;
+        }
+         main{
+            flex: 1;
+            overflow-y: auto;
+            background: blue;
+            
+        }
+        footer{
+            height: 2.5rem;
+            background: yellow;
+        }
+    }
+   
+   
+  
+</style>
+
+
