24b1b194d07ee000f74364cb723c050e39c5506a
lmlm <1269283359@qq.com>
2019-08-14 09:28:38 +0800

    ok

--- mi/src/components/Goodsdetail.vue
+++ mi/src/components/Goodsdetail.vue
@@ -1,6 +1,6 @@
 <template>
-<div>
-    <div class="menu"></div>
+<!-- <div> -->
+    <!-- <div class="menu"></div> -->
     <div id="goodsdetail">
         <div class="goods_detail" v-for="goods in songList" :key="goods.w_id">
             <div class="detail_swiper">
@@ -142,7 +142,7 @@
             </div>
         </div>
     </div> 
-    <!-- </div>  -->
+    <!-- </div> s -->
 </template>
 <script>
 // import service from '../services/service';
